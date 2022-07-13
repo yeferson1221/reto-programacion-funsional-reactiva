a80e16a802704f42306a1d2da7b51cb1579e3f6a
yeferson1221 <alejandro.yandd@gmail.com>
2022-07-13 18:11:37 -0500

    solo falta documentar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,30 @@
 import model.Email;
 import use.Case;
 import use.Chat;
+import use.Derivate;
 
 public class Main {
     public static void main(String[] args) {
        Case cas = new Case();
        Chat chat = new Chat();
+       Derivate derivate = new Derivate();
         System.out.println("Correos: " +cas.execute());
+        System.out.println("");
+        System.out.println("..................................................................................................................................................................................................................");
         System.out.println("Correos: " +cas.executeDistinct());
-        System.out.println("Correos: " +cas.executeFilter());
+        System.out.println("");
+        System.out.println("..................................................................................................................................................................................................................");
         System.out.println("Correos: " +cas.validateEmail());
+        System.out.println("");
+        System.out.println("..................................................................................................................................................................................................................");
         System.out.println("Correos: " +cas.validateEmailActiveAndDesactive());
+        System.out.println("");
+        System.out.println("..................................................................................................................................................................................................................");
+        System.out.println("la derivada numerica puntual en x=5 es: "+derivate.DerivateNumericaPuntual(5));
+        System.out.println("");
+        System.out.println("Integral: "+derivate.integralFuntion(5,6));
+        System.out.println("");
+        System.out.println("..................................................................................................................................................................................................................");
         chat.filterChat();
 
     }
--- src/main/java/use/Derivate.java
+++ src/main/java/use/Derivate.java
@@ -4,25 +4,26 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Derivate {
-
-
-    public String derivate(Double coefficient, Double exponent) {
-        BiFunction<Double, Double, Double> coeficienteResult = (coef, expo) -> coef * expo;
-        Function<Double, Double> exponenteResult = (expo) -> expo - 1;
-
-        return "la derivada de: " + coefficient +"x^" + exponent +" es de: " + coeficienteResult.apply(coefficient, exponent) +"x^" + exponenteResult.apply(exponent);
+    public static double f(double x){
+        return  (x*x) + (2*x)+1;
     }
-    //
-    public String integrate(Double coefficient, Double exponent) {
-        BiFunction<Double, Double, Double> coeficienteResult = (coef, expo) -> {
-            if (coef / (expo + 1) == 1) {
-                return 1.0;
-            } else {
-                return coef / (expo + 1);
-            }
-        } ;
-        Function<Double, Double> exponenteResult = (expo) -> expo + 1;
 
-        return "la integral de: ∫" + coefficient +"x^" + exponent +"dx es de: " + coeficienteResult.apply(coefficient, exponent) +"x^" + exponenteResult.apply(exponent) +"+ C";
+    public static double DerivateNumericaPuntual(double x){
+        double h = 1e-16;
+        double numerator = f(x+h)-f(x);
+        return (numerator/h);
+    }
+
+    public String integralFuntion(double coefficient, double exponent) {
+        BiFunction<Double, Double, Double> coefficientResult = (c, ex) -> {
+            if (c / (ex + 2) == 2) {
+                return 2.0;
+            } else {
+                return c / (ex + 2);
+            }
+        };
+        Function<Double, Double> exponenteResult = (expo) -> expo + 2;
+
+        return "la integral de: ∫" + coefficient + "x^" + exponent + "dx es de: " + coefficientResult.apply(coefficient, exponent) + "x^" + exponenteResult.apply(exponent) + "+ C";
     }
 }
